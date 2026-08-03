fc2ef55710eaddcfe193ce903268d048de599e60
౨ৎ ˖ ℒ𝒶𝓇𝓇𝓎 ℛℯ𝒾𝓈 ˖ ✟ <larrymiaumiau@gmail.com>
2026-08-03 17:28:18 -0300

    Delete Exercícios Prog. II Repetições e Arrays directory

--- Exercícios Prog. II Repetições e Arrays/Repetições/Ex3R/Ex3R.js
+++
@@ -1,8 +0,0 @@
-let soma = 0;
-let numero = 666;
-while (numero !== 0) {
-  numero = Number(prompt("Digite um número (ou 0 para encerrar):"));
-  soma = soma + numero;
-}
-
-console.log("A soma total é: " + soma);
